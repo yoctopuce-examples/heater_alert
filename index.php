16e3eac993217c5a2aca78ee66aff4d3c974f59a
Seb <sebastien@rinsoz.org>
2021-02-02 11:12:38 +0100

    minor fix

--- index.php
+++ index.php
@@ -1,46 +1,25 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-define("MD5_PASSWD","password");
-define("DEST_EMAIL","somebody@example.com");
-define("SRC_EMAIL","somebody@example.com");
-
-
+define("MD5_PASSWD", "password");
+define("DEST_EMAIL", "somebody@example.com");
+define("SRC_EMAIL", "somebody@example.com");
 
 
 function send_alert()
 {
-    $subject = "Chauffage en panne!";
-    $message = "
-<html>
-<head>
-<title>Chauffage en panne</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
+    $subject = "Heater Alert!";
+    $message = "<html><head><title>{$subject}</title></head><body><h1>{$subject}</h1><p>The Heater alert has been triggered. Please check it.</p></body></html>";
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-    $headers .= 'From: <'.SRC_EMAIL.'>' . "\r\n";
-    mail('DEST_EMAIL', $subject, $message, $headers);
+    $headers .= 'From: <' . SRC_EMAIL . '>' . "\r\n";
+    mail(DEST_EMAIL, $subject, $message, $headers);
 }
 
 
 $error = "";
-if (YAPI::TestHub('md5:'.MD5_PASSWD.'@callback', 10, $error) == YAPI::SUCCESS) {
-    YAPI::RegisterHub('md5:'.MD5_PASSWD.'@callback');
+if (YAPI::TestHub('md5:' . MD5_PASSWD . '@callback', 10, $error) == YAPI::SUCCESS) {
+    YAPI::RegisterHub('md5:' . MD5_PASSWD . '@callback');
     send_alert();
     die("Alarm has been triggered");
 }
